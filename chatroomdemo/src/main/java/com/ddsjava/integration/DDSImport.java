d3d7b9eda0f17aa0358009bcede80d2ee1ebc680
Alexxigang <37231458+Alexxigang@users.noreply.github.com>
2018-11-22 23:15:31 +0800

    Add files via upload

---
+++ chatroomdemo/src/main/java/com/ddsjava/integration/DDSImport.java
@@ -0,0 +1,13 @@
+package com.ddsjava.integration;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+
+public interface DDSImport extends Library {
+    DDSImport instance = (DDSImport) Native.load("dds", DDSImport.class);
+
+    public int CalcDDtablePBN(DDTableDealPBN.ByValue tableDealPBN, DDTableResults.ByReference tableResults);
+
+    public int SolveBoardPBN(DDDealPBN.ByValue dealPBN, int target, int solutions, int mode, DDFutureTricks.ByReference futureTricks, int threadIndex);
+}
